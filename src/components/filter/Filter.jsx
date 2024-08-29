a2c70c7e9055c692782d668ae0a9a7425c1c1749
shahid ali <beingshahidali@gmail.com>
2024-08-29 10:00:52 +0530

    [UBN-009] Listpage search ready

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,6 +1,74 @@
 import "./filter.scss";
 
 function Filter() {
-  return <div className="filter">Filter</div>;
+  return (
+    <div className="filter">
+      <h1>
+        Search results for <b>London </b>
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City Location"
+          ></input>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="buy">any</option>
+            <option value="buy">Buy</option>
+            <option value="ent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select id="property" name="property">
+            <option value="buy">any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="input"
+            id="minPrice"
+            name="minPrice"
+            placeholder="any"
+          ></input>
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="any"
+          ></input>
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input
+            type="number"
+            id="bedroom"
+            name="bedroom"
+            placeholder="any"
+          ></input>
+        </div>
+        <button>
+          <img src="/search.png" alt="serch icon" />
+        </button>
+      </div>
+    </div>
+  );
 }
 export default Filter;
