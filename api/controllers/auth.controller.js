054a8905e982e71c012cfdd208bdda7d5a4f3ef7
shahid ali <beingshahidali@gmail.com>
2024-09-25 16:58:17 +0530

    Cookies added while loggin a user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const register = async (req, res) => {
       },
     });
 
-    res.status(201).send("Registered successfully");
+    res.status(201).send({ message: "User registered successfully" });
   } catch (error) {
     console.error("Error registering user:", error);
     res
@@ -23,9 +23,28 @@ export const register = async (req, res) => {
       .send("An error occurred while registering user: " + error.message);
   }
 };
-export const login = (req, res) => {
-  console.log("login");
-  res.send("login");
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username: username },
+    });
+    if (!user) return res.status(401).send({ message: "Invalid credentials" });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).send({ message: "Invalid password" });
+    // res.setHeader("Set-cookie", "test=" + "myValue").send("Successfully ");
+    res
+      .cookie("user", "myTestValue", {
+        httpOnly: true,
+        //  secure: true,
+        maxAge: 1000000,
+      })
+      .status(200)
+      .send({ message: "Successfully logged in" });
+  } catch (e) {
+    res.status(500).send("An error occurred while logging in: " + e.message);
+  }
 };
 export const logout = (req, res) => {
   console.log("logout");
