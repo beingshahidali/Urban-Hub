8fd631e40a5758d2552bd9e76a05db574e96b99c
shahid ali <beingshahidali@gmail.com>
2024-09-25 16:24:04 +0530

    Registering new using using Prisma

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,33 @@
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-  res.send("Registered successfully");
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).send("Registered successfully");
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res
+      .status(500)
+      .send("An error occurred while registering user: " + error.message);
+  }
 };
 export const login = (req, res) => {
-  //db operation
   console.log("login");
   res.send("login");
 };
 export const logout = (req, res) => {
-  //db operation
   console.log("logout");
   res.send("logout");
 };
