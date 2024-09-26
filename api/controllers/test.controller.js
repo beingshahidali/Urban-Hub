bf7f5c0bd2a038b83bec37aefe22afb3b2333270
shahid ali <beingshahidali@gmail.com>
2024-09-26 10:05:53 +0530

    Middleare added

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,12 +1,6 @@
 import jwt from "jsonwebtoken";
 export const shouldBeLoggedIn = async (req, res) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({ message: "not authorized" });
-  }
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: "Token is invalid" });
-  });
+  console.log(req.userId);
   return res.status(200).json({ message: "You are authenticated" });
 };
 export const shouldBeAdmin = async (req, res) => {
