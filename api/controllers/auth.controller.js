3ae8bca218a6459dcc3a940472aee4dac5bd3896
shahid ali <beingshahidali@gmail.com>
2024-09-26 09:45:34 +0530

    Added protection to routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -41,6 +41,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       {
         id: user.id,
+        isAdmin: true,
       },
       process.env.JWT_SECRET_KEY,
       { expiresIn: age }
---
+++ api/controllers/test.controller.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+export const shouldBeLoggedIn = async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is invalid" });
+  });
+  return res.status(200).json({ message: "You are authenticated" });
+};
+export const shouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is invalid" });
+    if (!payload.isAdmin)
+      return res.status(403).json({ message: "Not an Admin" });
+  });
+  return res.status(200).json({ message: "You are authenticated" });
+};
