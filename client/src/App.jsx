7299960938e2a9c1a5820a86917a9e48e6180c1e
shahid ali <beingshahidali@gmail.com>
2024-09-28 10:53:06 +0530

    Single Page loader created and made image upload to accept multiple Files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./routes/profilePage/profilePage";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import NewPostPage from "./routes/newPostPage/newPostPage";
+import { singlePageLoader } from "./lib/loaders";
 function App() {
   const router = createBrowserRouter([
     {
@@ -25,6 +26,7 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage />,
+          loader: singlePageLoader,
         },
 
         {
